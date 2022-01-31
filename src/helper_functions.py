906d175b97b1135cef86ce97b58558c09dee4c1b
Cat6498 <59997464+Cat6498@users.noreply.github.com>
2022-01-31 12:08:08 +0000

    Fixing variable names
    Fixed some variable names and default values to make the evaluation function easier

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -21,12 +21,12 @@ def get_background_color(inp_img):
   else:
     return "black"
 
-def evaluate_result(target, result, baseline, picture_name, weights, map, type, method="direct"):
-  target = cv2.imread("input/doggo.jpg", cv2.IMREAD_COLOR)
-  result = cv2.imread("output/doggo/doggo_final.png", cv2.IMREAD_COLOR)
+def evaluate_result(inp_img, res_name, baseline, picture_name, weights, use_map, trans_type=0, method="direct"):
+  target = cv2.imread(inp_img, cv2.IMREAD_COLOR)
+  result = cv2.imread(res_name, cv2.IMREAD_COLOR)
   target = cv2.resize(target, (result.shape[1], result.shape[0]))
 
-  if type != 2: 
+  if trans_type != 2: 
     mse = np.mean((result - target)**2)
     mrse = np.sqrt(mse)
 
@@ -36,30 +36,30 @@ def evaluate_result(target, result, baseline, picture_name, weights, map, type, 
 
     ssim = structural_similarity(result, target, data_range=target.max() - target.min(), multichannel=True)
 
-    if type == 0: # normal painting
-      data = pd.DataFrame({"image name":[picture_name], "baseline": [baseline], "weights":[weights], "map":[map], 
+    if trans_type == 0: # normal painting
+      data = pd.DataFrame({"image name":[picture_name], "baseline": [baseline], "weights":[weights], "map":[mapb], 
               "mse":[mse], "mrse":[mrse], "l1":[l1], "psnr":[psnr], "ssim":[ssim]})
       data.to_csv("./results.csv", index=False, mode='a', header=None)
 
-    elif type == 1: # style transfer
-      data = pd.DataFrame({"image name":[picture_name], "baseline": [baseline], "method":[method], "weights":[weights], "map":[map], 
+    elif trans_type == 1: # style transfer
+      data = pd.DataFrame({"image name":[picture_name], "baseline": [baseline], "method":[method], "weights":[weights], "map":[mapb], 
               "mse":[mse], "mrse":[mrse], "l1":[l1], "psnr":[psnr], "ssim":[ssim]})
       data.to_csv("./results_style.csv", index=False, mode='a', header=None)
     
   else: # only on masked objects
-    map_name = "maps/" + picture_name + "/" + map + ".jpg"
-    map = cv2.imread(map_name, cv2.IMREAD_GRAYSCALE)
-    map = cv2.resize(map, (result.shape[1], result.shape[0]))
+    map_name = "maps/" + picture_name + "/" + use_map + ".jpg"
+    mapb = cv2.imread(map_name, cv2.IMREAD_GRAYSCALE)
+    mapb = cv2.resize(mapb, (result.shape[1], result.shape[0]))
 
-    mse = np.mean((result[map != 0] - target[map != 0])**2)
+    mse = np.mean((result[mapb != 0] - target[mapb != 0])**2)
     mrse = np.sqrt(mse)
 
-    l1 = np.mean(np.abs(result[map != 0] - target[map != 0]))
+    l1 = np.mean(np.abs(result[mapb != 0] - target[mapb != 0]))
 
     psnr = 20 * np.log10(255.0/mrse)
 
-    ssim = structural_similarity(result[map != 0], target[map != 0], data_range=target.max() - target.min(), multichannel=True)
+    ssim = structural_similarity(result[mapb != 0], target[mapb != 0], data_range=target.max() - target.min(), multichannel=True)
 
-    data = pd.DataFrame({"image name":[picture_name], "baseline": [baseline], "weights":[weights], "map":[map], 
+    data = pd.DataFrame({"image name":[picture_name], "baseline": [baseline], "weights":[weights], "map":[mapb], 
               "mse":[mse], "mrse":[mrse], "l1":[l1], "psnr":[psnr], "ssim":[ssim]})
     data.to_csv("./results_objects.csv", index=False, mode='a', header=None)
